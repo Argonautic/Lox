a79a94672a0ad7a0b6b18fbfe8575f37bf33cf60
Argonautic <jonathan.zhou93@gmail.com>
2019-04-01 08:08:58 -0400

    Add functionality for recursive descent parser and definitions for various kinds of expressions

---
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,78 @@
+/* Class to represent basic mathematical expressions in Bacchus Naur form. Uses Visitor pattern - most
+ * operations for these Expressions will be stored as external Visitor classes (like functions) instead
+ * of as methods on these expressions. They will simply get access to the Expression's properties and
+ * return some value.
+ * 
+ * This makes it harder to add new types because you have to add the corresponding visit method to each
+ * existing Visitor, but it makes it easy to add new operations. This works well for this phase of the
+ * interpreter.*/
+
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+abstract class Expr {
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+
+    abstract <R> R accept(Visitor<R> visitor);
+
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+
+    static class Grouping extends Expr {
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
+        final Expr expression;
+    }
+
+    static class Literal extends Expr {
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+
+        final Object value;
+    }
+
+    static class Unary extends Expr {
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+
+        final Token operator;
+        final Expr right;
+    }
+
+}
--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -1,3 +1,8 @@
+/*
+ * 1. Determine how Lox is receiving source code
+ * 2. Scan source code either line by line (interactive) or as a file
+ * 3. Parse tokens received from source code into usable data structures
+ */
 
 package com.craftinginterpreters.lox;
 
@@ -47,14 +52,25 @@ public class Lox {
 		Scanner scanner = new Scanner(source);
 		List<Token> tokens= scanner.scanTokens();
 		
-		// For now just print the tokens
-		for (Token token: tokens) {
-			System.out.println(token);
-		}
+		Parser parser = new Parser(tokens);                    
+		Expr expression = parser.parse();
+
+	    // Stop if there was a syntax error.                   
+	    if (hadError) return;                                  
+
+	    System.out.println(new AstPrinter().print(expression));
 	}
 	
 	static void error(int line, String message) {
 		report(line, "", message);
+	}
+	
+	static void error(Token token, String message) {
+		if (token.type == TokenType.EOF) {
+			report(token.line, " at end", message);
+		} else {
+			report(token.line, " at '" + token.lexeme + "'", message);
+		}
 	}
 	
 	private static void report(int line, String where, String message) {
