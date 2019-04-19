cb4bb2bc41454f6c6dc961f88255d1e94b0a3328
Argonautic <jonathan.zhou93@gmail.com>
2019-04-19 08:32:25 -0400

    add support for closures

--- src/com/craftinginterpreters/lox/LoxFunction.java
+++ src/com/craftinginterpreters/lox/LoxFunction.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment closure) {
+        this.closure = closure;
         this.declaration = declaration;
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);  // Tied to interpreter.globals instead of interpreter.environent because functions get access to global vars but not necessarily vars of callee scope
+        Environment environment = new Environment(closure);  // Tied to interpreter.globals instead of interpreter.environent because functions get access to global vars but not necessarily vars of callee scope
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
