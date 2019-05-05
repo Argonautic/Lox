2f780095349e7d320adcf25f6a0a7e6c0dd0facf
Argonautic <jonathan.zhou93@gmail.com>
2019-05-05 19:10:04 -0400

    support for passing arguments to instance constructors

--- src/com/craftinginterpreters/lox/LoxClass.java
+++ src/com/craftinginterpreters/lox/LoxClass.java
@@ -25,14 +25,22 @@ class LoxClass implements LoxCallable {  // LoxCallable for the constructors
         return name;
     }
 
+    // Initialize
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
         LoxInstance instance = new LoxInstance(this);
+        LoxFunction initializer = findMethod("init");
+        if (initializer != null) {
+            initializer.bind(instance).call(interpreter, arguments);
+        }
+
         return instance;
     }
 
     @Override
     public int arity() {
-        return 0;  // TODO: change when fleshing out constructors
+        LoxFunction initializer = findMethod("init");
+        if (initializer== null) return 0;
+        return initializer.arity();
     }
 }
--- src/com/craftinginterpreters/lox/LoxInstance.java
+++ src/com/craftinginterpreters/lox/LoxInstance.java
@@ -7,28 +7,6 @@ class LoxInstance {
     private LoxClass klass;
     private final Map<String, Object> fields = new HashMap<>();
 
-    LoxInstance(LoxClass klass) {
-        this.klass = klass;
-    }
-
-    // LoxInstance getting and setting can be done dynamically because the resolver ensures
-    // you'll always get the intended instance every time you refer to an instance name
-    Object get(Token name) {
-        if (fields.containsKey(name.lexeme)) {
-            return fields.get(name.lexeme);
-        }
-
-        LoxFunction method = klass.findMethod(name.lexeme);
-        if (method != null) return method.bind(this);
-
-        // Piss off, javascript
-        throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
-    }
-
-    void set(Token name, Object value) {
-        fields.put(name.lexeme, value);
-    }
-
     /*
         Lox uses bound methods, which means that when methods are called, they refer to the instance they
         were *drawn* from. Normally, since methods are drawn (using '.') at the same time they're called
@@ -53,6 +31,28 @@ class LoxInstance {
 
         Within methods, the keyword "this" will always refer to the instance the method was drawn from as well
      */
+
+    LoxInstance(LoxClass klass) {
+        this.klass = klass;
+    }
+
+    // LoxInstance getting and setting can be done dynamically because the resolver ensures
+    // you'll always get the intended instance every time you refer to an instance name
+    Object get(Token name) {
+        if (fields.containsKey(name.lexeme)) {
+            return fields.get(name.lexeme);
+        }
+
+        LoxFunction method = klass.findMethod(name.lexeme);
+        if (method != null) return method.bind(this);
+
+        // Piss off, javascript
+        throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
+    }
+
+    void set(Token name, Object value) {
+        fields.put(name.lexeme, value);
+    }
 
     @Override
     public String toString() {
